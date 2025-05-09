14ce73423b4c32337d8961198ed2854e58ec2223
EmPuiquin24 <hepuiquin@proton.me>
2025-05-09 00:41:07 -0500

    hola

---
+++ src/main/java/com/example/demopc01/Demopc01Application.java
@@ -0,0 +1,13 @@
+package com.example.demopc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demopc01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demopc01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demopc01/Demopc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopc01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demopc01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
